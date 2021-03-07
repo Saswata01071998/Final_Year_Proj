28413241e0b2ec9a725ca3c26dca4236e99c4254
Saswata01071998 <saswatadas222@gmail.com>
2021-03-08 00:15:21 +0530

    login added

---
+++ app/src/main/java/com/appsnipp/education/ui/fragments/Login.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2021. rogergcc
+ */
+
+package com.appsnipp.education.ui.fragments;
+
+import androidx.fragment.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+
+import com.appsnipp.education.R;
+
+public class Login extends Fragment {
+
+    public Login() {
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.login_page, container, false);
+    }
+}
